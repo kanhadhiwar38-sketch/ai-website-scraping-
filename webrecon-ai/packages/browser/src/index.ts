74a255f305dd873d9604ea0337e62fd3baf65a63
kanhadhiwar38-sketch <kanhadhiwar38@gmail.com>
2026-09-05 20:28:29 +0530

    kanha

---
+++ webrecon-ai/packages/browser/src/index.ts
@@ -0,0 +1,6 @@
+export {
+  BrowserSessionManager,
+  getBrowserSessionManager,
+  VIEWPORT_PRESETS,
+} from "./session-manager.js";
+export type { CreateSessionInput, SessionInfo } from "./session-manager.js";
