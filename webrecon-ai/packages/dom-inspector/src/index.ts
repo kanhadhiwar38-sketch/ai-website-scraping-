e2d32174f3bdb9537a7a4224047002e7ef4ddcf2
kanhadhiwar38-sketch <kanhadhiwar38@gmail.com>
2026-09-05 20:28:34 +0530

    kanha

---
+++ webrecon-ai/packages/dom-inspector/src/index.ts
@@ -0,0 +1,10 @@
+export { extractDomSnapshot } from "./extractor.js";
+export type { DomExtractionResult, DomExtractionOptions } from "./extractor.js";
+
+export { extractLinks } from "./links.js";
+export type { ExtractedLink } from "./links.js";
+
+export { extractRenderedHtml } from "./html.js";
+
+export { normalizeElements } from "./normalizer.js";
+export type { NormalizeOptions } from "./normalizer.js";
