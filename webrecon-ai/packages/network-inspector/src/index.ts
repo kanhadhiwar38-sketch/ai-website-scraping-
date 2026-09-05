8dcfcafbbcad5f42b2328a51630859759a62c5f4
kanhadhiwar38-sketch <kanhadhiwar38@gmail.com>
2026-09-05 20:28:52 +0530

    kanha

---
+++ webrecon-ai/packages/network-inspector/src/index.ts
@@ -0,0 +1,7 @@
+export { NetworkCapture } from "./capture.js";
+export type { CapturedNetworkRequest } from "./capture.js";
+
+export { classifyResourceType } from "./resource-classifier.js";
+
+export { summarizeApiRequests, inferSchema } from "./api-summary.js";
+export type { ApiSummaryEntry, JsonSchemaShape } from "./api-summary.js";
