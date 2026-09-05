db1ac479a8c3ceb968fd5b8063b5b9ce9b5e82df
kanhadhiwar38-sketch <kanhadhiwar38@gmail.com>
2026-09-05 20:28:17 +0530

    kanha

---
+++ webrecon-ai/packages/asset-analyzer/src/index.ts
@@ -0,0 +1,8 @@
+export {
+  discoverAssets,
+  discoverAssetsFromDom,
+  enrichAssetsFromNetwork,
+} from "./discover.js";
+export type { DiscoveredAsset } from "./discover.js";
+
+export { storeAssets } from "./asset-store.js";
